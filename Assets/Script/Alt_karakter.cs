71819454eabc150d4456f3e955140e5c51a6620a
Kronos <kastul_91@hotmail.com>
2026-02-13 16:55:50 +0300

    started

---
+++ Assets/Script/Alt_karakter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alt_karakter : MonoBehaviour
+{
+    GameObject Target;
+    NavMeshAgent _Navmesh;
+    void Awake()
+    {
+        _Navmesh = GetComponent<NavMeshAgent>();
+        Target = GameObject.FindWithTag("GameManager").GetComponent<GameManager>().VarisNoktasi;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        _Navmesh.SetDestination(Target.transform.position);
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Hedef;
+    public GameObject PrefabKarakter;
+    public GameObject DogmaNoktasi;
+    public GameObject VarisNoktasi;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            Instantiate(PrefabKarakter,DogmaNoktasi.transform.position, Quaternion.identity);
+    }
+}
